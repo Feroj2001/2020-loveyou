12986230ef7d63699595aba901084ef54b9c8e51
Feroj <khapangiferoj@gmail.com>
2020-10-02 21:40:39 -0600

    add if statement for the pompt

--- Develop/script.js
+++ Develop/script.js
@@ -23,50 +23,9 @@ function generatePassword() {
   var passwordLength = prompt("How many characters do you want the password to be? The password cannot be less than 8 or greater than 128");
 
   if (passwordLength < 8 || passwordLength > 128) {
-    alert("The password cannot be less than 8 or greater than 128.")
-    passwordLength = prompt("How many characters do you want the password to be? The password cannot be less than 8 or greater than 8");
+    alert("The password cannot be less than 8 or greater than 128");
+    passwordLength = prompt("How many characters do you want the password to be? The password cannot be less than 8 or greater than 128");
   }
-
-
-
-
-  var confirmLowerCase = confirm("Do you want to add loweracase on you password?");
-  var confirmUpperCase = confirm("Do you want to add uppercase on our password?");
-  var confirmNumeric = confirm("Do you want nuumbers on your password?");
-  var confirmSpecial = confirm("Do you want special characters on your password?");
-
-  if (confirmLowerCase === false && confirmUpperCase === false && confirmNumeric === false && confirmSpecial === false) {
-    alert("You must choose at least one option provided to you")
-    confirmLowerCase = confirm("Do you want to add loweracase on you password?");
-    confirmUpperCase = confirm("Do you want to add uppercase on our password?");
-    confirmNumeric = confirm("Do you want nuumbers on your password?");
-    confirmSpecial = confirm("Do you want special characters on your password?");
-  }
-
-  if (confirmLowerCase) {
-    storingVar = storingVar.concat(lowerCaseCharacter)
-  }
-
-  if (confirmUpperCase) {
-    storingVar = storingVar.concat(upperCaseCharacter)
-  }
-
-  if (confirmNumeric) {
-    storingVar = storingVar.concat(numberCharacter)
-  }
-
-
-  if (confirmSpecial) {
-    storingVar = storingVar.concat(specialCharacter)
-  }
-
-  var randomPassword = "";
-
-  for (var i = 0; i < passwordLength; i++) {
-    randomPassword = randomPassword + storingVar[Math.floor(Math.random() * storingVar.length)];
-  }
-
-  return randomPassword;
 
 
 }
